7062a5ff21cbbe42fcf43050179383bf31a48a6b
YuwenLi11 <31485092+YuwenLi11@users.noreply.github.com>
2018-05-29 14:58:26 -0700

    path filter finished

--- disease.py
+++ disease.py
@@ -9,7 +9,12 @@ from pyspider.libs.base_handler import *
 class Handler(BaseHandler):
     crawl_config = {
     }
-
+    
+#    def __initial__(self):
+#        self.category = ''
+#        self.disease_name = ''
+        
+        
     @every(minutes=24 * 60)
     def on_start(self):
         self.crawl('http://disease.medlive.cn/wiki/list/171', callback=self.index_page)
@@ -17,11 +22,14 @@ class Handler(BaseHandler):
     @config(age=10 * 24 * 60 * 60)
     def index_page(self, response):
         for each in response.doc('ul.sortListA a').items():
+            cate = response.doc('ul.sortListA a').text()
             self.crawl(each.attr.href, callback=self.list_page)
             
     def list_page(self, response):
+        name = response.doc('.case_name > label').text()
         for each in response.doc('dd a').items():
-            self.crawl(each.attr.href, callback=self.general_page)
+            if each.text().find('已完成') != -1:
+                self.crawl(each.attr.href, callback=self.general_page)
     
     def general_page(self, response):
         for each in response.doc('.case_name > a').items():
@@ -29,7 +37,8 @@ class Handler(BaseHandler):
     
     def disease_page(self, response):
         for each in response.doc('dd > a').items():
-            self.crawl(each.attr.href, callback=self.detail_page)
+            if each.hasClass('nodata') != 1:
+                self.crawl(each.attr.href, callback=self.detail_page)
             
     
     @config(priority=2)
