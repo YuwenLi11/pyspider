088c760def726a7f3c616dd5c5da78ac862ed648
YuwenLi11 <31485092+YuwenLi11@users.noreply.github.com>
2018-06-15 18:03:06 -0700

    update csv reader

--- csv_reader.py
+++ csv_reader.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
-import codecs
-import pymysql
-import json
 import csv
 def main():
     ####
     # each company's name is result[0] in the for loop
     ###
-    f = open("/Users/liyuwen/Desktop/test.csv", "r")
-
-    for line in f:
-        line = f.readline()
-        result = line.split(',')
-        print(result[0])
+    f = open("/Users/liyuwen/Desktop/pyspider/companies_data.csv", "r")
+    reader = csv.reader(f)
+    for line in reader:
+        result = line[0]
+        print(result)
 
 if __name__ == '__main__':
     main()
