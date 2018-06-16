ebbff46332377d3a2dcf4df23aa64e37e3d610b2
YUWEN LI <31485092+YuwenLi11@users.noreply.github.com>
2018-06-15 18:04:02 -0700

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -2,7 +2,7 @@
 import csv
 def main():
     ####
-    # each company's name is result[0] in the for loop
+    # each company's name is result in the for loop
     ###
     f = open("/Users/liyuwen/Desktop/pyspider/companies_data.csv", "r")
     reader = csv.reader(f)
