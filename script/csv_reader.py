a85a031f358f06b0c669b0e12740b6742720456e
YuwenLi11 <31485092+YuwenLi11@users.noreply.github.com>
2018-06-21 12:18:51 -0700

    name issue

---
+++ script/csv_reader.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import csv
+def main():
+    ####
+    # each company's name is result in the for loop
+    ###
+    f = open("/Users/liyuwen/Desktop/pyspider/companies_data.csv", "r")
+    reader = csv.reader(f)
+    for line in reader:
+        result = line[0]
+        print(result)
+
+if __name__ == '__main__':
+    main()
